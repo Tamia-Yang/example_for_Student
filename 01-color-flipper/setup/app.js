05db678c5fcd182302c7212a637bb15bd22f951a
Tamia-Yang <76608164+Tamia-Yang@users.noreply.github.com>
2022-10-02 00:09:27 -0700

    color-flipper
    js example 01 - background color change

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,12 @@
 
+const btn = document.querySelector("button");
+
+btn.addEventListener("click",cangeColor);
+
+function cangeColor(){
+    let ColorNumber = Math.random().toString(16).substring(2,8);
+    document.body.style.backgroundColor = "#" + ColorNumber ;
+    document.querySelector("span").innerHTML ="#" + ColorNumber ;
+}
+
+
