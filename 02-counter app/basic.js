2665d00ee67d408b706d53f9fc7dc27affe425d5
Tamia-Yang <76608164+Tamia-Yang@users.noreply.github.com>
2022-10-03 23:27:40 -0700

    Update basic.js

--- 02-counter app/basic.js
+++ 02-counter app/basic.js
@@ -4,9 +4,24 @@ let result = document.querySelector(".count");
 btn.addEventListener("click",(e)=>{
     if(e.target.classList.contains("add")){
         result.innerHTML++;
+        
+
     }else if(e.target.classList.contains("sub")){
         result.innerHTML--;
+        
+       
     }else{
         result.innerHTML = 0;
     }
+    setColor()
 })
+
+function setColor(){
+    if(result.innerHTML == 0){
+        result.style.color = "white";
+    }else if(result.innerHTML < 0 ){
+        result.style.color = "red";
+    }else{
+        result.style.color = "green";
+    }
+}
