2e106d472611975509c10e56a6fa326257942987
Tamia-Yang <76608164+Tamia-Yang@users.noreply.github.com>
2022-10-04 22:24:34 -0700

    js modify

--- 04-expanding-cards/basic.js
+++ 04-expanding-cards/basic.js
@@ -1,19 +1,4 @@
-
-const btn = document.querySelector("button");
-
-btn.addEventListener("click",function(){
-    const copyText = document.getElementById("maintext");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // for mobile devices
-    
-    navigator.clipboard.writeText(copyText.value);
-    btn.textContent = "copyed!!";
-
-    setTimeout(()=>{
-        btn.textContent = "copy";
-    },1000)
-   
-
-   
-})
-
+$(".panel").click(function () {
+    $(".panel").removeClass("active");
+    $(this).addClass("active");   
+});
