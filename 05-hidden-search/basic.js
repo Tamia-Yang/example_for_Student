4eb5122f48cf827629a44a8519c391b96fcfa3e0
Tamia-Yang <76608164+Tamia-Yang@users.noreply.github.com>
2022-10-05 00:58:36 -0700

    hidden-search animation

---
+++ 05-hidden-search/basic.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".btn");
+const search = document.querySelector(".search");
+
+btn.addEventListener("click",function(){
+    search.classList.toggle("active");
+})
