288522d0dde1b8d75aa82d463359b45bb1164722
Tamia-Yang <76608164+Tamia-Yang@users.noreply.github.com>
2022-10-07 11:45:59 -0700

    animation effect
    html, css

--- 07-nav animation/basic.js
+++
@@ -1,9 +0,0 @@
-$(".split").on("mouseover",function(){
-    if($(this).hasClass("right")){
-        $(".right").css("width", "100%");
-        $(".left").css("width", "50%");
-    }else if($(this).hasClass("left")) {
-        $(".left").css("width", "100%");
-        $(".right").css("width", "50%");
-    }
-})
