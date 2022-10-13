cf3c45cd528e3d8cda7fdd8a1dd92a93153db81e
Tamia-Yang <76608164+Tamia-Yang@users.noreply.github.com>
2022-10-13 01:57:12 -0700

    jquery

--- 15-tabMenu with jquery/basic.js
+++ 15-tabMenu with jquery/basic.js
@@ -1,4 +1,9 @@
 $(".btn li").click(function(){
   $(this).addClass("active");
   $(this).siblings().removeClass("active");
+  let numb = $(this).index();
+  // console.log(numb);  
+  $(".tab").removeClass("active");
+  $(".tab").eq(numb).addClass("active");
+
 })
