abc0ea76735d5cebd7857f58f4f9eac0e541e541
Tamia-Yang <76608164+Tamia-Yang@users.noreply.github.com>
2022-10-14 13:00:53 -0700

    jquery clean up

--- 20-M-Library/basic.js
+++ 20-M-Library/basic.js
@@ -1,9 +1,11 @@
 
 $(".content").eq(0).show();
-$(".accordion-item .title").click(function(){
-  $(this).siblings(".content").slideUp();
+$(".accordion .title").click(function(){
+
   $(this).next().stop().slideToggle(300);
+  $(this).addClass("active");
 })
 $(".content").click(function(){
   $(this).stop().slideUp();
+ 
 })
