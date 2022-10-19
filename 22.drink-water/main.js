02596e02904112c0210a097b69291c1c4c50c8bf
Tamia-Yang <76608164+Tamia-Yang@users.noreply.github.com>
2022-10-20 01:12:30 +0900

    javascript

--- 22.drink-water/main.js
+++ 22.drink-water/main.js
@@ -1,1 +1,36 @@
-var 학생1 = { name: 'kim', age: 20 }
+$(".cup-small").click(function () {
+
+    $(this).toggleClass("full");
+    $(this).prevAll().addClass("full");
+    $(this).nextAll().removeClass("full");
+    updateBigCup();
+})
+
+const percentage = document.querySelector(".percentage");
+const smallCups = document.querySelectorAll(".cup-small");
+const remained = document.querySelector(".remained");
+const totalCups = smallCups.length;
+const liters = document.querySelector("#liters");
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll(".cup-small.full").length;
+    // console.log(fullCups);
+    if (fullCups == 0) {
+        remained.style.visibility = "visible";
+        percentage.style.visibility = "hidden";
+        percentage.style.height = 0;
+    } else if (fullCups === totalCups) {
+        remained.style.visibility = "hidden";
+        percentage.style.visibility = "visible";
+        remained.style.height = 0;
+        percentage.style.height = `${fullCups / totalCups * 330}px`;
+        percentage.innerText = `${fullCups / totalCups * 100}%`;
+    } else {
+        remained.style.visibility = "visible";
+        percentage.style.visibility = "visible";
+        percentage.style.height = `${fullCups / totalCups * 330}px`;
+        percentage.innerText = `${fullCups / totalCups * 100}%`;
+    }
+    liters.innerText = `${2 - (250 * fullCups / 1000)}L`
+
+}
